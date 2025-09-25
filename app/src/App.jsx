b4c984ec1e278659dcafa4c617c0f22fe4b1ce2c
danicolms <hello@danicolms.xyz>
2025-09-25 14:59:27 +0200

    feat: drag a square

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,12 +1,34 @@
+import { useState } from 'react';
 import { Stage, Layer, Rect } from 'react-konva';
 
 function App() {
+  const [position, setPosition] = useState({ x: 200, y: 200 });
+
+  console.log(window.innerHeight, window.innerWidth)
   return (
-    <Stage width={window.innerWidth} height={window.innerHeight}>
-      <Layer>
-        <Rect x={20} y={50} width={100} height={100} fill="red" shadowBlur={5} />
-      </Layer>
-    </Stage>
+    <div className='container'>
+      <Stage width={700} height={500} className='frame'>
+        <Layer>
+          <Rect x={position.x} y={position.y} width={100} height={100} fill="blue" draggable
+            onMouseEnter={(e) => {
+              document.body.style.cursor = 'grab';
+            }}
+            onMouseLeave={(e) => {
+              document.body.style.cursor = 'default';
+            }}
+            onDragStart={(e) => {
+              document.body.style.cursor = 'move';
+            }}
+            onDragEnd={(e) => {
+              document.body.style.cursor = 'grab';
+              setPosition({
+                x: e.target.x(),
+                y: e.target.y()
+              });
+            }} />
+        </Layer>
+      </Stage>
+    </div>
   );
 }
 
