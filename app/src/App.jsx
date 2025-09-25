27de8dde1de9ac11ee29435ac6e62e5c26834448
danicolms <hello@danicolms.xyz>
2025-09-25 14:43:45 +0200

    chore: konvajs

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,10 +1,13 @@
+import { Stage, Layer, Rect } from 'react-konva';
 
-export default function App() {
-
+function App() {
   return (
-    <>
-
-    </>
-  )
+    <Stage width={window.innerWidth} height={window.innerHeight}>
+      <Layer>
+        <Rect x={20} y={50} width={100} height={100} fill="red" shadowBlur={5} />
+      </Layer>
+    </Stage>
+  );
 }
 
+export default App;
