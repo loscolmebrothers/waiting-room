5d230e7f0c49cc1ff4d4c162502f393f58e30f4b
Daniel Colmenares Salamanca <danicolms@Daniels-MacBook-Air.local>
2025-10-08 11:06:30 +0200

    wip: animate QUICK text

--- src/Intro.jsx
+++ src/Intro.jsx
@@ -1,44 +1,52 @@
 import { Group, Layer, Rect, Text } from "react-konva";
 import useWindowSize from "./useWindowSize";
-import { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useSpring, animated, to } from "@react-spring/konva";
 
 function Intro({ finish }) {
   const { width, height } = useWindowSize();
   const [countdown, setCountdown] = useState(5);
 
-  const AnimatedRect = animated.Rect;
-  const AnimatedLayer = animated.Layer;
-  const AnimatedText = animated.Text;
-
-  const [fadeAnimation, fadeApi] = useSpring(() => ({
-    from: { opacity: 0 },
-    to: { opacity: 1 },
+  const [layerFadeOutAnimation, layerFadeOutAnimationApi] = useSpring(() => ({
+    from: { opacity: 1 },
     config: { tension: 200, friction: 20 },
   }));
 
-  const [gradientAnimation] = useSpring(() => ({
+  const [backgroundGreyscaleAnimation] = useSpring(() => ({
     from: { offset: 0 },
     to: { offset: 3 },
     loop: true,
     config: { duration: 600 },
   }));
 
-  const [quickAnimation] = useSpring(() => ({
-    from: { opacity: 0, scale: 0.5 },
+  const hasQuickTextAnimatedRef = useRef(false);
+
+  const [quickTextAnimation, quickTextAnimationApi] = useSpring(() => ({
+    from: { opacity: 0, scale: 100 },
+    to: [
+      { opacity: 1, scale: 2 },
+      { opacity: 1, scale: 1 },
+    ],
+    config: { tension: 200, friction: 20 },
+    onStart: () => {
+      hasQuickTextAnimatedRef.current = true;
+    },
+  }));
+
+  useEffect(() => {
+    if (hasQuickTextAnimatedRef.current) {
+      quickTextAnimationApi.stop();
+    }
+  }, [countdown, quickTextAnimationApi]);
+
+  const [escapeTextAnimation] = useSpring(() => ({
+    from: { opacity: 0, scale: 0.6 },
     to: { opacity: 1, scale: 1 },
+    delay: 1000,
     config: { tension: 200, friction: 20 },
   }));
 
-  const [escapeAnimation] = useSpring(() => ({
-    from: { opacity: 0, scale: 0.5 },
-    to: { opacity: 1, scale: 1 },
-    delay: 700,
-    config: { tension: 200, friction: 20 },
-  }));
-
-  const [countdownAnimation] = useSpring(() => ({
+  const [countdownTextAnimation] = useSpring(() => ({
     from: { opacity: 0, scale: 0.5 },
     to: { opacity: 1, scale: 1 },
     delay: 2000,
@@ -52,66 +60,93 @@ function Intro({ finish }) {
       }, 1000);
       return () => clearTimeout(timer);
     } else {
-      fadeApi.start({
+      layerFadeOutAnimationApi.start({
         opacity: 0,
-        config: { duration: 200 },
+        config: { duration: 100 },
         onRest: () => finish(true),
       });
     }
-  }, [countdown, finish, fadeApi]);
+  }, [countdown, finish, layerFadeOutAnimationApi]);
 
-  const backgroundColor = to([gradientAnimation.offset], (offset) => {
-    const wave = Math.sin(offset * Math.PI * 2) * 0.5 + 0.5;
-    const gray = Math.floor(5 + wave * 10);
-    return `rgb(${gray}, ${gray}, ${gray})`;
-  });
+  const greyscaleBackgroundColor = to(
+    [backgroundGreyscaleAnimation.offset],
+    (offset) => {
+      const wave = Math.sin(offset * Math.PI * 2) * 0.5 + 0.5;
+      const gray = Math.floor(5 + wave * 10);
+      return `rgb(${gray}, ${gray}, ${gray})`;
+    },
+  );
+
+  const AnimatedLayer = animated.Layer;
+  const AnimatedRect = animated.Rect;
+  const AnimatedGroup = animated.Group;
 
   return (
     <AnimatedLayer
       width={width}
       height={height}
-      opacity={fadeAnimation.opacity}
+      opacity={layerFadeOutAnimation.opacity}
     >
-      <AnimatedRect width={width} height={height} fill={backgroundColor} />
+      <AnimatedRect
+        width={width}
+        height={height}
+        fill={greyscaleBackgroundColor}
+      />
 
-      <AnimatedText
-        text="Quick!"
-        x={0}
+      <AnimatedGroup
+        x={width / 2}
         y={height / 2 - 20}
-        width={width}
-        fontSize={32}
-        fill="white"
-        align="center"
-        opacity={quickAnimation.opacity}
-        scaleX={quickAnimation.scale}
-        scaleY={quickAnimation.scale}
-      />
+        opacity={quickTextAnimation.opacity}
+        scaleX={quickTextAnimation.scale}
+        scaleY={quickTextAnimation.scale}
+      >
+        <Text
+          text="Quick!"
+          x={0}
+          y={0}
+          fontSize={32}
+          fill="white"
+          offsetX={50}
+          offsetY={16}
+        />
+      </AnimatedGroup>
 
-      <AnimatedText
-        text="Don't let them escape"
-        x={0}
+      <AnimatedGroup
+        x={width / 2}
         y={height / 2 + 20}
-        width={width}
-        fontSize={24}
-        fill="white"
-        align="center"
-        opacity={escapeAnimation.opacity}
-        scaleX={escapeAnimation.scale}
-        scaleY={escapeAnimation.scale}
-      />
-      <AnimatedText
-        text={countdown}
-        x={0}
+        opacity={escapeTextAnimation.opacity}
+        scaleX={escapeTextAnimation.scale}
+        scaleY={escapeTextAnimation.scale}
+      >
+        <Text
+          text="Don't let them escape"
+          x={0}
+          y={0}
+          fontSize={24}
+          fill="white"
+          offsetX={165}
+          offsetY={12}
+        />
+      </AnimatedGroup>
+
+      <AnimatedGroup
+        x={width / 2}
         y={height / 2 + 60}
-        width={width}
-        fontSize={48}
-        fill="white"
-        fontStyle="bold"
-        align="center"
-        opacity={countdownAnimation.opacity}
-        scaleX={countdownAnimation.scale}
-        scaleY={countdownAnimation.scale}
-      />
+        opacity={countdownTextAnimation.opacity}
+        scaleX={countdownTextAnimation.scale}
+        scaleY={countdownTextAnimation.scale}
+      >
+        <Text
+          text={countdown}
+          x={0}
+          y={0}
+          fontSize={48}
+          fill="white"
+          fontStyle="bold"
+          offsetX={15}
+          offsetY={24}
+        />
+      </AnimatedGroup>
     </AnimatedLayer>
   );
 }
